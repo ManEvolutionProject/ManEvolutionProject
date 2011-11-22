d407b172f8fc226fef646c04875db29b0c8f5375
yurig <yurig@yurig-A780X-A2.(none)>
2011-11-22 15:07:25 +0200

    some routing changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def menu_link text, url, item_name = nil
     link_to(content_tag(:span, "") + text, url, {:class => @current_item == item_name ? "current_section" : ""})
   end
